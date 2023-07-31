979f6886ba4903773ca2118cd44fb860a71c6f77
Chanveasna-ENG <137870404+Chanveasna-ENG@users.noreply.github.com>
2023-07-31 21:46:51 +0700

    Update exercise_4.py

--- exercise_4.py
+++ exercise_4.py
@@ -1,12 +1,12 @@
-def chunking_by(numbers, chunck):
+def chunking_by(numbers, chunk):
     new = []
 
-    if chunck == 0:
+    if chunk == 0:
         return numbers
 
-    while len(numbers) / chunck >= 1:
-        new.append(numbers[0:chunck])
-        del numbers[0:chunck]
+    while len(numbers) / chunk >= 1:
+        new.append(numbers[0:chunk])
+        del numbers[0:chunk]
     if len(numbers) > 0:
         new.append(numbers)
     return new
